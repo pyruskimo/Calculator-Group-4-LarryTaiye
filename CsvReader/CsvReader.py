d13738903bb38a039f5ddbe8b0b205c28332d37d
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-03 21:29:55 -0400

    Feature: Created CsvReader

---
+++ CsvReader/CsvReader.py
@@ -0,0 +1,27 @@
+import csv
+from Fileutilities.absolutepath import absolutepath
+from pprint import pprint
+
+
+def classFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
+
+
+class CsvReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []
+
+        with open(absolutepath(filepath)) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
+
+        pass
+
+    def return_data_as_class(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(ClassFactory(class_name, row))
+        return objects
