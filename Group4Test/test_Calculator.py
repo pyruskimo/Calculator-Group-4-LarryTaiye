6940a0dc62278b408de4710cd966ef4cfb4a3aec
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-05 22:21:28 -0400

    Fix: Updated code line 25 and 43

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -21,8 +21,9 @@ class MyTestCase(unittest.TestCase):
     def test_add_method_calculator(self):
         test_data = CsvReader("Group4Test/Group4Data/addition.csv").data
         for row in test_data:
-            self.assertEqual(self.calculator.add(row['Value 1']), row['Value 2']), float(row['Result'])
-            self.assertEqual(self.calculator.result, float(row['Result']))
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.add(row['Value 1']), row['Value 2']), (row['Result'])
+            self.assertEqual(self.calculator.result, (row['Result']))
 
    
     def test_multiply_method_calculator(self):
@@ -40,7 +41,7 @@ class MyTestCase(unittest.TestCase):
     def test_sq_method_calculator(self):
         test_data = CsvReader("Group4Test/Group4Data/square.csv").data
         for row in test_data:
-        result = float(row['Result'])
+            result = float(row['Result'])
             self.assertEqual(self.calculator.sq(row['Value 1']), (row['Result']))
             self.assertEqual(self.calculator.result, (row['Result']))
        
