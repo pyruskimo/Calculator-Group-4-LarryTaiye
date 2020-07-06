c86d80feb68f742427739e7427eb449b3b771b77
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-06 12:28:49 -0400

    Update test_statistics.py

--- Group4Test/test_statistics.py
+++ Group4Test/test_statistics.py
@@ -2,7 +2,7 @@ import unittest
 from numpy.random import seed
 from numpy.random import randint
 from Statistics.Statistics import Statistics
-from Statistics.Mean import Mean
+# from Statistics.Mean import Mean
 import pprint
 
 
@@ -19,25 +19,6 @@ class MyTestCase(unittest.TestCase):
         mean = self.statistics.mean(self.testData)
         self.assertEqual(mean, 4.25)
 
-    def test_median_calculator(self):
-        median = self.statistics.median(self.testData)
-        self.assertEqual(median, 4.25)  
-        
-    def test_mode_calculator(self):
-        median = self.statistics.mode(self.testData)
-        self.assertEqual(mode, 4.25)   
-        
-    def test_variance_calculator(self):
-        median = self.statistics.variance(self.testData)
-        self.assertEqual(variance, 4.25)       
-        
-    def test_stdev_calculator(self):
-        median = self.statistics.stdev(self.testData)
-        self.assertEqual(stdev, 4.25)        
-
-    def test_zscore_calculator(self):
-        median = self.statistics.zscore(self.testData)
-        self.assertEqual(zscore, 4.25)          
-
+   
 if __name__ == '__main__':
     unittest.main()
