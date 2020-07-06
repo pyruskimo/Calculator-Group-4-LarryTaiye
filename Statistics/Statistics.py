7425e6cbfd57d7601b3e521eeff8983d1a26dd0e
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-06 19:45:01 -0400

    Fix: Update Statistics.py

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,30 +1,30 @@
 from Calculator.Calculator import Calculator
-from Statistics.Mean import 
-from Statistics.Median import 
-from Statistics.Mode import 
-from Statistics.stdev import
-from Statistics.zscore impor
+from Statistics.Mean import Mean 
+from Statistics.Median import Median
+from Statistics.Mode import Mode
+from Statistics.stdev import stdev
+from Statistics.zscore import zscore
 
 
 class Statistics(Calculator):
 
     def mean(self, data):
-        self.result = mean(data)
+        self.result = Statistics.Mean(data)
         return self.result
         
     def median(self, data):
-        self.result = median(data)
+        self.result = Statistics.median(data)
         return self.result  
 
     def mode(self, data):
-        self.result = mode(data)
+        self.result = Statistics.mode(data)
         return self.result     
         
     def stdev(self, data):
-        self.result = stdev(data)
+        self.result = Statistics.stdev(data)
         return self.result  
 
     def zscore(self, data):
-        self.result = zscore(data)
+        self.result = Statistics.zscore(data)
         return self.result         
         
