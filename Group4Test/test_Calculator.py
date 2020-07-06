38a664119aea26aae02f8c533041136714b3db00
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-05 22:28:38 -0400

    Update test_Calculator.py

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -19,11 +19,11 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.result, float(row['Result']))
         
     def test_add_method_calculator(self):
-        test_data = CsvReader("Group4Test/Group4Data/addition.csv").data
-        for row in test_data:
-            result = float(row['Result'])
-            self.assertEqual(self.calculator.add(row['Value 1']), row['Value 2']), (row['Result'])
-            self.assertEqual(self.calculator.result, (row['Result']))
+    test_data = CsvReader("Group4Test/Group4Data/addition.csv").data
+    for row in test_data:
+        result = float(row['Result'])
+        self.assertEqual(self.calculator.add(row['Value 1']), row['Value 2']), (row['Result'])
+        self.assertEqual(self.calculator.result, (row['Result'])
 
    
     def test_multiply_method_calculator(self):
@@ -48,8 +48,9 @@ class MyTestCase(unittest.TestCase):
     def test_sqrt_method_calculator(self):
         test_data = CsvReader("Group4Test/Group4Data/squareroot.csv").data
         for row in test_data:
-            self.assertEqual(self.calculator.sqrt(row['Value 1']), float(row['Result'])
-            self.assertEqual(self.calculator.result, float(row['Result']))          
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.sqrt(row['Value 1']), (row['Result'])
+            self.assertEqual(self.calculator.result, (row['Result']))          
        
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
