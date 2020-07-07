e07f5b37728ad372eb1554ca4772a7bb15f70a45
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-06 21:09:19 -0400

    Fix: Update test_statistics.py

--- Group4Test/test_statistics.py
+++ Group4Test/test_statistics.py
@@ -10,7 +10,7 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         seed(5)
         self.testData = randint(0, 10, 20)
-        self.statistics = Statistics()
+        self.statistics = Statistics.mean()
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
