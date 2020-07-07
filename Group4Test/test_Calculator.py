d2b5fa4a6f572616f13a11046bd14c1fe49842df
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-07 19:57:48 -0400

    Fix: Update test_Calculator.py

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -36,7 +36,7 @@ class MyTestCase(unittest.TestCase):
         test_data = CsvReader("Group4Test/Group4Data/division.csv").data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.divide(row['Value 2'], row['Value 1']), result)
+            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)             
 
     def test_results_property(self):
