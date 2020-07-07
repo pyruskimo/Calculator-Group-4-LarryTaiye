83c14247edb6bbdd5f76361bb48c27519435b2b5
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-06 21:34:34 -0400

    Fix: Update test_statistics.py

--- Group4Test/test_statistics.py
+++ Group4Test/test_statistics.py
@@ -1,8 +1,7 @@
 import unittest
 from numpy.random import seed
 from numpy.random import randint
-# from Statistics.statistics import Statistics
-import statistics
+from Statistics.Statistics import Statistics
 import pprint
 
 
@@ -19,6 +18,6 @@ class MyTestCase(unittest.TestCase):
         mean = self.statistics.mean(self.testData)
         self.assertEqual(mean, 4.25)
 
-   
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    
