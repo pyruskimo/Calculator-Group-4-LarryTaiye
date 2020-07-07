782c0b3e86440de6fed95e3343640d00c3e62469
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-06 22:56:56 -0400

    Feature: Added division code path

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -25,7 +25,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)            
 
-           
+      def test_division(self):
+        test_data = CsvReader("Group4Test/Group4Data/division.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)           
             
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
