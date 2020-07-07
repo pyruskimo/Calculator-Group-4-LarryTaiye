6e047b9ee403434eff0954143ec81ce1243356ca
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-06 22:36:30 -0400

    Fix: Update test_Calculator.py

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -29,7 +29,7 @@ class MyTestCase(unittest.TestCase):
         test_data = CsvReader("Group4Test/Group4Data/multiplication.csv").data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.multiplication(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)              
             
     def test_results_property(self):
