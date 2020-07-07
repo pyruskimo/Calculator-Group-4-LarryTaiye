2bd3aafe2634707c841e355bbb82feef46163ff7
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-07 19:35:16 -0400

    Feature: Added definition for division

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -30,7 +30,14 @@ class MyTestCase(unittest.TestCase):
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
-            self.assertEqual(self.calculator.result, result)              
+            self.assertEqual(self.calculator.result, result)
+            
+    def test_division(self):
+        test_data = CsvReader("Group4Test/Group4Data/division.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)             
 
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
