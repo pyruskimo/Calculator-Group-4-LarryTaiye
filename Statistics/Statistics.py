50d8ef455f6993dd41640ce38d6b135ac1d26b27
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-07 18:03:28 -0400

    Fix: Update Statistics.py

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -2,7 +2,7 @@
 from Calculator.Calculator import Calculator
 # from Statistics.Mean import mean
 import statistics
-Calculator.Mutiplication import multiplication # Added due to - ModuleNotFoundError: No module named 'Calculator.Mutiplication'
+
 
 class Statistics(Calculator):
 
