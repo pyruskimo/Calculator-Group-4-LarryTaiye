926a8ad069ec1dc9cebdbf3acc843ef209303f02
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-07 19:13:41 -0400

    Update Mean.py

--- Statistics/Mean.py
+++ Statistics/Mean.py
@@ -2,7 +2,7 @@ from Calculator.Addition import addition
 from Calculator.Division import division
 from Calculator.Mutiplication import multiplication
 
-class Statistics(Calculator): # NameError: name 'mean' is not defined
+# class Statistics(Calculator): # NameError: name 'mean' is not defined
 
 def mean(data):
     num_values = len(data)
