a23f2f1227768eeed15c114e54f3709472a82b26
kj2gh <60633535+kj2gh@users.noreply.github.com>
2020-07-07 17:29:22 -0400

    Fix: Update test_statistics.py

--- Group4Test/test_statistics.py
+++ Group4Test/test_statistics.py
@@ -2,6 +2,7 @@ import unittest
 from numpy.random import seed
 from numpy.random import randint
 from Statistics.Statistics import Statistics
+# from statistics.mean import mean
 import pprint
 
 
@@ -9,7 +10,7 @@ class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
         seed(5)
         self.testData = randint(0, 10, 20)
-        self.statistics = Statistics()
+        self.statistics = Statistics.mean()
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
