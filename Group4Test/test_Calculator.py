8540ffe6692f7c94aa50854933bbc22d99cb788d
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-08 14:58:58 -0400

    Feature: Added squareroot definition

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -43,8 +43,15 @@ class MyTestCase(unittest.TestCase):
         test_data = CsvReader("Group4Test/Group4Data/square.csv").data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.square(row['Value 1'], result))
-           # self.assertEqual(self.calculator.result, result)        
+            self.assertEqual(self.calculator.square(row['Value 1'], result)
+            self.assertEqual(self.calculator.result, result)        
+
+    def test_squareroot(self):
+        test_data = CsvReader("Group4Test/Group4Data/squareroot.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.squareroot(row['Value 1'], result)
+            self.assertEqual(self.calculator.result, result)               
             
             
     def test_results_property(self):
