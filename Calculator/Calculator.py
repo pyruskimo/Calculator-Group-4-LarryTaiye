0c8d55869f013bfc3de2dfcb11b2c59272cd5335
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-08 17:39:52 -0400

    Fix: Update Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -2,6 +2,7 @@ from Calculator.Subtraction import subtraction
 from Calculator.Addition import addition
 from Calculator.Multiplication import multiplication
 from Calculator.Division import division
+from Calculator.sqrt import sqrt
 
 import math
 
