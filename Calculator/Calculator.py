d8247d3bfd8b2903159718e8fd9fc689f2c27a0a
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-08 15:09:25 -0400

    Fix: Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -29,7 +29,7 @@ class Calculator:
         return self.result  
 
     def square(self, a): 
-        self.result = square(a, a))
+        self.result = square(a, a)
         return self.result  
     
     def squareroot(self, base(a)):
