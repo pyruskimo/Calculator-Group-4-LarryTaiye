c4076265c2753fd278f1eae4434c6e3dad2b2801
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-08 22:13:23 -0400

    Update test_Calculator.py

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -55,8 +55,6 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.math.sqrt(row['Value 1'], result))
             self.assertEqual(self.calculator.result, result)              
             
- def wait_and_move_to(self, mode, id):
-    elem = self.wait_and_get(mode, id)
             
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
