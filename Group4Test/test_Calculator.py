b80515824b9fd1bd8c8ddf17146a147c45b6c3d3
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-13 12:45:40 -0400

    fix: Update test_Calculator.py

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -49,7 +49,7 @@ class MyTestCase(unittest.TestCase):
     def test_square_root(self):
         test_data = CsvReader("Group4Test/Group4Data/square_root.csv").data
         for row in test_data:
-            result = row(['Result'])
+            result = row['Result']
             #result = float(row['Result'], 9)
             #print(round((a ** .5), 10
             self.assertEqual(self.calculator.square_root(int(row['Value 1'])), result)
