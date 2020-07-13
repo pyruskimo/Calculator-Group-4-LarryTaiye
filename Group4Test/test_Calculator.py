f3c26ac86288328caf9df82d1641af31b1f84b2c
pyruskimo <60906233+pyruskimo@users.noreply.github.com>
2020-07-12 20:30:39 -0400

    Fix: Update test_Calculator.py

--- Group4Test/test_Calculator.py
+++ Group4Test/test_Calculator.py
@@ -1,6 +1,5 @@
 import unittest
-import math
-
+from math import sqrt
 from Calculator.Calculator import Calculator
 from CsvReader.CsvReader import CsvReader
 
@@ -18,44 +17,42 @@ class MyTestCase(unittest.TestCase):
             result = float(row['Result'])
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
-            
+
     def test_addition(self):
         test_data = CsvReader("Group4Test/Group4Data/addition.csv").data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), result)
-            self.assertEqual(self.calculator.result, result)    
-            
+            self.assertEqual(self.calculator.result, result)
+
     def test_multiplication(self):
         test_data = CsvReader("Group4Test/Group4Data/multiplication.csv").data
         for row in test_data:
             result = float(row['Result'])
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), result)
             self.assertEqual(self.calculator.result, result)
-            
+
     def test_division(self):
         test_data = CsvReader("Group4Test/Group4Data/division.csv").data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
-            self.assertEqual(self.calculator.result, float(row['Result']))         
-            
+            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), result)
+            self.assertEqual(self.calculator.result, result)
+
     def test_square(self):
         test_data = CsvReader("Group4Test/Group4Data/square.csv").data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual(row['Value 1'], result)
-            self.assertEqual(self.calculator.result, result)        
+            self.assertEqual(self.calculator.square(row['Value 1']), result)
+            self.assertEqual(self.calculator.result, result)
 
-    # Old values = def test_sqrt(self):
-    def test_sqrt(self):
-        test_data = CsvReader("Group4Test/Group4Data/squareroot.csv").data
+    def test_square_root(self):
+        test_data = CsvReader("Group4Test/Group4Data/square_root.csv").data
         for row in test_data:
             result = float(row['Result'])
-            self.assertEqual((row['Value 1'], result))
-            self.assertEqual(self.calculator.result, result)              
-            
-            
+            self.assertEqual(self.calculator.square_root(row['Value 1']), result)
+            self.assertEqual(self.calculator.result, result)
+
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
 
